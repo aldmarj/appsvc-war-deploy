4642180364b018279f56998e5227529e13a20ca1
Aldmar <aldmarj@users.noreply.github.com>
2023-07-19 11:08:41 -0500

    Update HomeController.java

--- src/main/java/com/devops/azure/HomeController.java
+++ src/main/java/com/devops/azure/HomeController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    String message = "This is a war file from Azure DevOps pipelines!";
+    String message = "This is a war file from GitHub Actions pipelines!";
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String index() {
         return message;
     }
