62fb6aeee1fbd40edfc0801fd47731ffa7acf1d4
Aldmar Joubert <ajoubert@microsoft.com>
2023-07-19 11:27:18 -0500

    added redirection and downgraded spring boot version

--- src/main/java/com/devops/azure/HomeController.java
+++ src/main/java/com/devops/azure/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     String message = "This is a war file from Azure DevOps pipelines!";
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String index() {
         return message;
     }
---
+++ src/main/java/com/devops/azure/RedirectController.java
@@ -0,0 +1,22 @@
+package com.devops.azure;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+@Controller
+@RequestMapping("/")
+public class RedirectController {
+    
+    @GetMapping("/")
+    public RedirectView redirectWithUsingRedirectView(
+      RedirectAttributes attributes) {
+        attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
+        return new RedirectView("/home");
+    }
+}
+    
+
